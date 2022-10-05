67f3f6139ca5f09505b71e7723c365b473dcdee8
braverokmc79 <braverokmc79@gmail.com>
2022-10-05 11:59:56 +0900

    69. 회원가입 UI

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ var template = require('../lib/template.js');
 
 
 module.exports = function (passport) {
+
   router.get('/login', function (request, response) {
 
     var fmsg = request.flash();
@@ -25,6 +26,33 @@ module.exports = function (passport) {
     `, '');
     response.send(html);
   });
+
+
+  router.get('/register', function (request, response) {
+
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    var title = 'WEB - register';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+      <div style="color:red;">${feedback}</div>
+      <form action="/auth/register_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p><input type="password" name="pwd2" placeholder="password"></p>
+        <p><input type="text" name="displayName" placeholder="display name"></p>
+        <p>
+          <input type="submit" value="register">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+  });
+
+
 
   router.post('/login_process',
     passport.authenticate('local', {
